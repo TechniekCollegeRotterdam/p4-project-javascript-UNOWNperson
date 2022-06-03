a94ed931421852e811b79804c47290d38f6768ac
Maik Alexander Lüdeck <9016083@student.tcrmbo.nl>
2022-06-03 13:42:08 +0200

    testpush

---
+++ pauzescherm.js
@@ -0,0 +1,1 @@
+//dit wordt de javascript code voor het pauzescherm hier kan kiezen of je verder gaat of day je terug naar startscherm gaat
---
+++ spel.js
@@ -0,0 +1,3 @@
+// hier komt de code te staan voor het spel zelf 
+//namelijk het bijhouden score, vragen stellen in discriptie of met foto, A B C D antwoorden,balk waar je je antwoord kunt typen, 
+//bijhouden wanneer spel eindigd en tonen of het antwoord goed of fout is en als het fout is het goede antwoord laten voor dat de speler naar de volgende vraag gaat
